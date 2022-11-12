483f73c7bdebd63821bd39050646387d402bbf10
Nandini <juicyfruit5600@yahoo.ca>
2022-11-11 19:44:32 -0500

    Added all Activities

---
+++ app/src/main/java/com/example/nandinihariprasad_tatsianaptushko_comp304401_lab4_ex1/UpdateInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.nandinihariprasad_tatsianaptushko_comp304401_lab4_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_info);
+    }
+}
---
+++ app/src/main/java/com/example/nandinihariprasad_tatsianaptushko_comp304401_lab4_ex1/ViewTestInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.nandinihariprasad_tatsianaptushko_comp304401_lab4_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewTestInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_test_info);
+    }
+}
